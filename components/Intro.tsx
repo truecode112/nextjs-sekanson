bd830eae288b29b70c6423ea7333f5feb11fa5b7
truecode112 <truecoder112@gmail.com>
2023-02-01 05:32:08 +0900

    Fixed landing page

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,26 +1,20 @@
 import { Yeseva_One } from "@next/font/google";
 import clsx from "clsx";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import { useListen } from "../hooks/useListen";
 import { useMetamask } from "../hooks/useMetamask";
 import Head from "next/head";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import Image from 'next/image'
 
 type Props = {};
 
 
 const Intro = (props: Props) => {
-    const router = useRouter()
-    const { connected } = useAppContext()
-    const { currentChainId, setCurrentChainId } = useAppContext()
     const { dispatch, state: { status, isMetamaskInstalled, wallet, balance }, } = useMetamask();
-    const listen = useListen();
-    const showInstallMetamask = status !== "pageNotLoaded" && !isMetamaskInstalled;
-    const showConnectButton = status !== "pageNotLoaded" && isMetamaskInstalled && !wallet;
-    const isConnected = status !== "pageNotLoaded" && typeof wallet === "string";
-
+    const [play, setPlay] = useState(false)
     const handleConnect = async () => {
         dispatch({ type: "loading" });
         const accounts = await window.ethereum.request({
@@ -28,16 +22,29 @@ const Intro = (props: Props) => {
         });
 
         if (accounts.length > 0) {
-            const sdk = new ThirdwebSDK("goerli");
-            const edition = await sdk.getContract("0xEe58A930D719469eCa78A63f62a1064d6b686d49", "nft-collection");
-            const balance = await edition.balanceOf(accounts[0]);
-            
-            if (balance.eq(0)) {
-                alert('No NFT on this account');
-                //return window.location.href = "/";
-            } else {
-                window.location.href = "/main"
+            try {
+                const sdk = new ThirdwebSDK("goerli");
+                const edition = await sdk.getContract("0xEe58A930D719469eCa78A63f62a1064d6b686d49", "nft-collection");
+                const balance = await edition.balanceOf(accounts[0]);
+                
+                if (balance.eq(0)) {
+                    //alert('No NFT on this account');
+                    //return window.location.href = "/";
+                    setPlay(true);
+                    setTimeout(() => {
+                        setPlay(false)
+                      }, 3000)
+                } else {
+                    window.location.href = "/main"
+                }
+            } catch (e) {
+                console.log(e);
+                setPlay(true);
+                setTimeout(() => {
+                    setPlay(false)
+                  }, 3000)
             }
+            
         }
     };
 
@@ -50,10 +57,22 @@ const Intro = (props: Props) => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className="intro">
+                    {play == true ? (
+                        <Image 
+                            src="/images/intro.gif" 
+                            alt=""
+                            fill/>
+                    ) : (
+                        <Image
+                            src="/images/intro_still.png"
+                            alt=""
+                            fill
+                        />
+                    )}
+                
                 <button 
                     className="intro_connect" 
                     onClick={handleConnect}>
-
                 </button>
             </div>
         </section>
